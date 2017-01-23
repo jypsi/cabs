04dc23872a4270df5417a879cca8c6bde33fbb49
Ratnadeep Debnath <rtnpro@gmail.com>
2017-01-23 17:33:29 +0530

    Improved admin pages.

--- opencabs/admin.py
+++ opencabs/admin.py
@@ -2,8 +2,31 @@ from django.contrib import admin
 
 from .models import Booking, Place, Rate, VehicleCategory, VehicleFeature
 
-admin.site.register(Booking)
-admin.site.register(Place)
-admin.site.register(Rate)
-admin.site.register(VehicleCategory)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('pnr', 'customer_name', 'customer_mobile',
+                    'source', 'destination', 'booking_type',
+                    'travel_datetime', 'vehicle_category', 'status')
+    list_filter = ('booking_type', 'vehicle_category', 'status')
+    search_fields = ('pnr', 'customer_name', 'customer_mobile')
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = ('source', 'destination', 'vehicle_category',
+                    'oneway_price', 'driver_charge')
+    list_filter = ('vehicle_category',)
+
+
+@admin.register(VehicleCategory)
+class VehicleCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tariff_per_km', 'tariff_after_hours')
+    list_filter = ('features',)
+
 admin.site.register(VehicleFeature)
