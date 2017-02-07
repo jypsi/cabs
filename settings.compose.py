5737d4adac972bfd8d1e37494b4e6d12f981003e
Ratnadeep Debnath <rtnpro@gmail.com>
2017-02-07 13:58:45 +0530

    Added docker compose file.

---
+++ settings.compose.py
@@ -0,0 +1,13 @@
+import os
+from opencabs.default_settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASS', 'postgres'),
+        'HOST': 'db',
+        'PORT': 5432
+    }
+}
