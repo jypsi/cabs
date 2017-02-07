72da1f995063fc1193f4d74695a9f9573b1dcd5a
Ratnadeep Debnath <rtnpro@gmail.com>
2017-02-07 21:20:58 +0530

    Added STATIC config in settings and docker compose file.

--- opencabs/default_settings.py
+++ opencabs/default_settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get(
+    'STATIC_ROOT', os.path.join(os.path.dirname(__file__), '../static'))
 
 BOOTSTRAP3 = {
     'set_required': False,  # For Django <= 1.8 only
