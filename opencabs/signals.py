b4b760cd38f32ac3f1d823c2e9067ba147998ae1
Ratnadeep Debnath <rtnpro@gmail.com>
2018-01-10 12:13:04 +0530

    Bugfix in deleting a booking.

--- opencabs/signals.py
+++ opencabs/signals.py
@@ -8,4 +8,5 @@ from finance.models import Payment
 def update_booking_payment_info(sender, instance, **kwargs):
     if instance.item_content_type.app_label == 'opencabs' and \
             instance.item_content_type.model == 'booking':
-        instance.item_object.update_payment_summary()
+        if instance.item_object:
+            instance.item_object.update_payment_summary()
