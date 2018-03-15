ff891757d35b534c567dfc8c18e19374fbe87b90
Ratnadeep Debnath <rtnpro@gmail.com>
2018-03-15 21:23:46 +0530

    Allow overriding templates from base dir's templates dir.

--- opencabs/default_settings.py
+++ opencabs/default_settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'opencabs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
