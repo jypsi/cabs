5719077836341ce672e236e40294e0074a114bce
Ratnadeep Debnath <rtnpro@gmail.com>
2018-03-24 14:15:38 +0530

    Add created field & filter in payment admin page.

--- finance/admin.py
+++ finance/admin.py
@@ -6,5 +6,5 @@ from .models import Payment
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('amount', 'type', 'mode', 'timestamp',
-                    'item_object', 'reference_id', 'comment')
-    list_filter = ('mode',)
+                    'item_object', 'reference_id', 'comment', 'created')
+    list_filter = ('mode', 'created')
