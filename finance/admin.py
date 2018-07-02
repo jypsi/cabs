75d195081e5b61052337ebac22120a4dc7b2a1b0
Ratnadeep Debnath <rtnpro@gmail.com>
2018-07-02 20:50:14 +0530

    Show customer name in payment admin list view.

--- finance/admin.py
+++ finance/admin.py
@@ -5,14 +5,17 @@ from .models import Payment
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('amount', 'type', 'mode', 'timestamp',
-                    'item_object', 'reference_id', 'comment', 'created')
+    list_display = ('amount', 'type', 'mode', 'timestamp', 'customer',
+                    'item_object', 'reference_id', 'comment', 'created',)
     list_filter = ('mode', 'created')
     search_fields = ('bookings__booking_id',)
 
     def item_object(self, obj):
         return '<a href="{}">{}</a>'.format(obj.item_object.get_admin_url(),
                                             obj.item_object)
+
+    def customer(self, obj):
+        return obj.item_object.customer_name
 
 
     item_object.allow_tags = True
