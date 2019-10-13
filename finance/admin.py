5d4d03d24f5f05caa6fbaf5b2864d38749105eb8
Ratnadeep Debnath <rtnpro@gmail.com>
2019-10-13 21:44:45 +0530

    Add a few booking fields in payments admin page and allow search.

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib import admin
 from django.utils import timezone
 
@@ -8,7 +10,18 @@ from .models import Payment
 class PaymentAdmin(admin.ModelAdmin):
     list_filter = ('created_by', 'mode', 'created_by', 'accounts_verified',
                    'accounts_last_updated_by', 'accounts_last_updated')
-    search_fields = ('created', 'timestamp')
+    search_fields = ('created', 'timestamp', 'bookings__booking_id',
+                     'bookings__customer_name')
+
+    def booking(self, obj):
+        return '<a href="{}">{}</a>'.format(obj.item_object.get_admin_url(),
+                                            obj.item_object)
+
+    def travel_datetime(self, obj):
+        return datetime.combine(obj.item_object.travel_date, obj.item_object.travel_time)
+
+    def customer_name(self, obj):
+        return obj.item_object.customer_name
 
     def save_model(self, request, obj, form, change):
         if not obj.id or obj.created_by is None:
@@ -28,9 +41,11 @@ class PaymentAdmin(admin.ModelAdmin):
         return fields
 
     def get_list_display(self, request):
-        fields = ['invoice_id', 'mode', 'status', 'created_by', 'created', 'amount', 'comment']
+        fields = ['invoice_id', 'booking', 'customer_name', 'travel_datetime', 'mode', 'status', 'amount', 'reference_id', 'comment']
         if request.user.has_perm('finance.verify_payment'):
-            fields.extend(['accounts_verified', 'accounts_received', 'accounts_comment', 'accounts_due'])
+            fields.extend(['accounts_verified', 'accounts_received', 'accounts_due', 'accounts_comment',])
             self.list_editable = ['accounts_verified', 'accounts_received', 'accounts_comment']
-        fields.extend(['accounts_last_updated_by', 'accounts_last_updated'])
+        fields.extend(['created_by', 'created', 'accounts_last_updated_by', 'accounts_last_updated'])
         return fields
+
+    booking.allow_tags = True
